c8d0a3891a4c38142830c8315c8193f4e83d1771
malek <malek50000@gmail.com>
2021-02-23 17:21:07 +0200

    add new java script file

---
+++ daynamic.js
@@ -0,0 +1,34 @@
+// confirm ("welcome to my cars website");
+
+// to welcome guest 
+var username =prompt("what is your name?")
+var age = prompt ("how old are you ?")
+if (age >= 18) {alert("welcome here")
+} 
+else{
+    alert("You need your parents to continue!")
+} 
+
+document.getElementById('touser').innerText= 'welcome ' + username + ' to our webiste'
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
